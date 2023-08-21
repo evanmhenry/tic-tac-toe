3acb7b633e8888dccf5f502683d1042fe1991995
evanmhenry <evanmhenry@gmail.com>
2023-08-21 14:30:15 -0400

    improvement #2

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -36,21 +36,22 @@ const Board = ({ xIsNext, squares, onPlay }: { xIsNext: boolean; squares: string
 		<>
 			<div className='status'>{status}</div>
 
-			<div className='board-row'>
-				<Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-				<Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-				<Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-			</div>
-			<div className='board-row'>
-				<Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-				<Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-				<Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-			</div>
-			<div className='board-row'>
-				<Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-				<Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-				<Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-			</div>
+			{(() => {
+				const board = []
+				for (let i = 0; i < 3; i += 1) {
+					const row = []
+					for (let j = 0; j < 3; j += 1) {
+						const index = i * 3 + j
+						row.push(<Square key={index} value={squares[index]} onSquareClick={() => handleClick(index)} />)
+					}
+					board.push(
+						<div key={i} className='board-row'>
+							{row}
+						</div>,
+					)
+				}
+				return board
+			})()}
 		</>
 	)
 }
